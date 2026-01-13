5ff48c4e6c5090330ead46f45cf17bf91fa12e9d
Paul Zupan <paul.zupan.13@gmail.com>
2026-01-12 21:37:48 -0800

    No more callbakcs

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
 #include <cstdint>
 #include <cstring>
 
-#define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
 
-/* We will use this renderer to draw into this window every frame. */
 static SDL_Window* window = NULL;
 static SDL_Renderer* renderer = NULL;
 
@@ -14,8 +11,7 @@ static constexpr std::size_t screen_height = 90;
 static std::uint32_t screen[screen_width * screen_height];
 static SDL_Texture* texture = nullptr;
 
-/* This function runs once at startup. */
-SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
+int main()
 {
     SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
 
@@ -45,41 +41,31 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
         screen[i] = 0xFF0000FF;
     }
 
-    return SDL_APP_CONTINUE; /* carry on with the program! */
-}
-
-/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
-SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
-{
-    if (event->type == SDL_EVENT_QUIT)
+    bool running = true;
+    while (running)
     {
-        return SDL_APP_SUCCESS; /* end the program, reporting success to the OS. */
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                running = false;
+            }
+        }
+
+        int texture_pitch = 0;
+        void* texture_pixels = nullptr;
+        if (!SDL_LockTexture(texture, nullptr, &texture_pixels, &texture_pitch))
+        {
+            SDL_Log("Failed to lock texture: %s\n", SDL_GetError());
+            return SDL_APP_FAILURE;
+        }
+
+        std::memcpy(texture_pixels, screen, texture_pitch * screen_height);
+        SDL_UnlockTexture(texture);
+
+        SDL_RenderClear(renderer);
+        SDL_RenderTexture(renderer, texture, nullptr, nullptr);
+        SDL_RenderPresent(renderer);
     }
-    return SDL_APP_CONTINUE; /* carry on with the program! */
-}
-
-SDL_AppResult SDL_AppIterate(void* appstate)
-{
-    int texture_pitch = 0;
-    void *texture_pixels = nullptr;
-    if (!SDL_LockTexture(texture, nullptr, &texture_pixels, &texture_pitch))
-    {
-        SDL_Log("Failed to lock texture: %s\n", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
-
-    std::memcpy(texture_pixels, screen, texture_pitch * screen_height);
-    SDL_UnlockTexture(texture);
-
-    SDL_RenderClear(renderer);
-    SDL_RenderTexture(renderer, texture, nullptr, nullptr);
-    SDL_RenderPresent(renderer);
-
-    return SDL_APP_CONTINUE; /* carry on with the program! */
-}
-
-/* This function runs once at shutdown. */
-void SDL_AppQuit(void* appstate, SDL_AppResult result)
-{
-    /* SDL will clean up the window/renderer for us. */
 }
